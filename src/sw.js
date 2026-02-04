fc007c416061f6003d4bb8b1aa0001b2e2feb100
minamar7 <139824491+minamar7@users.noreply.github.com>
2026-02-04 02:05:15 +0200

    Create src/sw.js

---
+++ src/sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "safee-cache-v1";
+const urlsToCache = ["/","/index.html","/src/app.js","/styles.css"];
+
+self.addEventListener("install", e=> e.waitUntil(
+  caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+));
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>res||fetch(e.request))
+  );
+});
